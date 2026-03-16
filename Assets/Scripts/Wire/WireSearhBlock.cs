854f8ca09cd396144e6cd9239f929107ab53c4cc
SirJohn <jogernaut10@gmail.com>
2026-03-16 09:19:54 +0700

    Не сохраненная функция спавна

--- Assets/Scripts/Wire/WireSearhBlock.cs
+++ Assets/Scripts/Wire/WireSearhBlock.cs
@@ -11,11 +11,12 @@ public class WireSearh : MonoBehaviour
     [SerializeField] PlaceScript place;
     private Tile CurrentTile;
     Vector3Int PositionStart;
+    Vector3Int Castom;
     Vector3Int PositionEnd;
 
     void Update()
     {
-        if (place.CurrentTile != null&& place.tiles[PlaceScript.tileid] == tile[0] && MoneySystem.isAvailable()) 
+        if (place.CurrentTile != null && place.tiles[PlaceScript.tileid] == tile[0] && MoneySystem.isAvailable())
         {
 
             PositionStart = place.GetTilePositionFromMouse();
@@ -23,14 +24,38 @@ public class WireSearh : MonoBehaviour
             //Debug.Log(PositionStart);
             if (Input.GetMouseButton(0))
             {
-                if(PositionEnd!= place.GetTilePositionFromMouse()){
+                if (PositionEnd != place.GetTilePositionFromMouse())
+                {
+                    Castom = place.GetTilePositionFromMouse();
+
+                    if (place.GetTilePositionFromMouse().x > PositionEnd.x)
+                    {
+
+                        Spawn(tileMap, tile[0], Castom, 0);
+                    }
+                    if (place.GetTilePositionFromMouse().x < PositionEnd.x)
+                    {
+
+                        Spawn(tileMap, tile[0], Castom, 180);
+                    }
+
                     PositionEnd = place.GetTilePositionFromMouse();
 
-                    Debug.Log(PositionEnd);
-                    place.PlaceTileAtMousePosition(PositionEnd, tile[0], tileMap); 
+                    //Debug.Log(PositionEnd);
                 }
             }
         }
     }
+
+    void Spawn(Tilemap map, Tile tile, Vector3Int position, int Degre)
+    {
+        map.SetTile(position, null);
+        place.PlaceTileAtMousePosition(position, tile, tileMap);
+        Matrix4x4 matrix = Matrix4x4.TRS(Vector3.zero, Quaternion.Euler(0f, 0f, Degre), Vector3.one);
+        map.SetTransformMatrix(position, matrix);
+
+    }
+
+
 
 }
